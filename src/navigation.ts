3f0a8c0fa68d55dea784f35e84e4b3bf54634a82
mazen-ahmed <mazenreal22@gmail.com>
2024-08-16 20:41:21 +0300

    locales error

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,7 +1,7 @@
 import { createSharedPathnamesNavigation } from "next-intl/navigation";
 
-export const locales = ["en", "ar"];
-export const localePrefix = "always"; // Default
+export const locales = ["ar", "en"];
+export const localePrefix = "as-needed"; // Default
 
 export const { Link, redirect, usePathname, useRouter } =
 	createSharedPathnamesNavigation({ locales, localePrefix });
