502953fc314e3bb82a696f158d053e1908bb48d5
mazen-ahmed <mazenreal22@gmail.com>
2024-08-18 00:29:02 +0300

    removed header text

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -4,9 +4,6 @@ import React from "react";
 const Header = ({ translation }: { translation: any }) => {
 	return (
 		<div className="header flex items-center justify-center">
-			<h1 className=" md:text-5xl text-center font-bold text-white text-[24px] md:text-[40px]">
-				{translation.bestPlatform}
-			</h1>
 			<div>{/* <SearchInput /> */}</div>
 		</div>
 	);
