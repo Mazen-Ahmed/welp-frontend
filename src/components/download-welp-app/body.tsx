20e008aa53ed17f3a2174f7d50a998b28b4bb227
mazen-ahmed <mazenreal22@gmail.com>
2024-09-07 20:10:17 +0300

    handled download app issue

--- src/components/download-welp-app/body.tsx
+++ src/components/download-welp-app/body.tsx
@@ -2,26 +2,42 @@
 
 import { useTranslations } from "next-intl";
 import Image from "next/image";
-import { useEffect } from "react";
-import React from "react";
+import { useEffect, useState } from "react";
 
 const DownloadWelpAppBody = ({ deviceType }: { deviceType: string }) => {
+	const [wasRedirected, setWasRedirected] = useState(false);
+
 	const downloadWelpAppT = useTranslations("downloadWelpApp");
 
 	useEffect(() => {
-		if (deviceType === "android") {
-			window.location.href =
-				"https://play.google.com/store/apps/details?id=com.welp.welp";
-		} else {
-			window.location.href =
-				"https://apps.apple.com/us/app/welp-rating-social-reviews/id6478454000";
+		if (!wasRedirected) {
+			if (deviceType === "android") {
+				window.location.href =
+					"https://play.google.com/store/apps/details?id=com.welp.welp";
+				setWasRedirected(true);
+			} else {
+				window.location.href =
+					"https://apps.apple.com/us/app/welp-rating-social-reviews/id6478454000";
+				setWasRedirected(true);
+			}
 		}
-	}, [deviceType]);
+	}, [deviceType, wasRedirected]);
+
+	console.log(wasRedirected);
 
 	return (
 		<div className="flex flex-col items-center justify-center">
 			<Image src="/logo.svg" width={150} height={150} alt="logo" />
-			{downloadWelpAppT("redirect")}
+			{!wasRedirected && downloadWelpAppT("redirect")}
+			{wasRedirected && (
+				<div>
+					<p
+						className="underline "
+						onClick={() => setWasRedirected(false)}>
+						{downloadWelpAppT("didNotRedirect")}
+					</p>
+				</div>
+			)}
 		</div>
 	);
 };
